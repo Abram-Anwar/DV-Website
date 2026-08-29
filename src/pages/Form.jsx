5e6b17adb34b0e8bb8e5bbc8d89e4052da01284e
Abram-Anwar <ibram.a.nassif@gmail.com>
2026-08-29 16:09:40 +0300

    Update form

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -2,18 +2,59 @@ import PersonalInfo from "../components/Form/PersonalInfo";
 import SpouseInfo from "../components/Form/SpouseInfo";
 import ChildrenInfo from "../components/Form/ChildrenInfo";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const Form = () => {
   const { state } = useLocation();
   const { status, hasChildren } = state;
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = [
+    {
+      title: "البيانات الشخصية",
+      component: <PersonalInfo />,
+    },
+
+    ...(status === "married"
+      ? [
+          {
+            title: "بيانات الزوج/الزوجة",
+            component: <SpouseInfo />,
+          },
+        ]
+      : []),
+
+    ...(status === "married" && hasChildren === "yes"
+      ? [
+          {
+            title: "بيانات الأطفال",
+            component: <ChildrenInfo />,
+          },
+        ]
+      : []),
+
+    { title: "المراجعة", component: <div>Review</div> },
+  ];
 
   return (
     <>
-      <PersonalInfo />
+      {steps[currentStep].component}
 
-      {status === "married" && <SpouseInfo />}
+      <div>
+        <button
+          onClick={() => setCurrentStep(currentStep - 1)}
+          disabled={currentStep === 0}
+        >
+          السابق
+        </button>
 
-      {status === "married" && hasChildren === "yes" && <ChildrenInfo />}
+        <button
+          onClick={() => setCurrentStep(currentStep + 1)}
+          disabled={currentStep === steps.length - 1}
+        >
+          التالي
+        </button>
+      </div>
     </>
   );
 };
